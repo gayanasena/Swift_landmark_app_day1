4883918d31de773ab91577a3f83f12c3dfdab972
Gayana Senarathne <76644097+gayanasena@users.noreply.github.com>
2022-10-19 16:51:20 +0530

    Day01

---
+++ MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by Stephen Innovations on 2022-10-18.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
